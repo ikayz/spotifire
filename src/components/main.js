74efd0b34d0b3aa3568eec3859eeec2b19c7e6e2
mandrigo <manderat206@gmail.com>
2018-10-20 15:48:31 +0200

    added registration form

--- src/components/main.js
+++ src/components/main.js
@@ -4,6 +4,19 @@ class Main extends Component {
   render() {
     return (
       <main>
+     <div class="side-box"> 
+      
+       <h2 class="sidebar-headings">side bar </h2>
+       <ul>
+          prevention
+          
+       </ul>
+      
+     
+       </div>   
+
+        
+           
       </main>
     );
   }
